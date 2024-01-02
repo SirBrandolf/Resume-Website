4d90a397a7bfc20877ab8bd85b81e1b6dfd0bf89
Brandon Wang <brandolfthewang@gmail.com>
2024-01-02 00:38:58 -0500

    Added Backend and Updated Front Page
    Added:
    - Sidebar
    - Linkedin Button
    - Background Image
    - About me section
    - List of technical skills

---
+++ backend.js
@@ -0,0 +1,7 @@
+document.getElementById('linkedin').addEventListener('click', function() {
+    window.location.href = 'https://www.linkedin.com/in/brandonwang05/'; 
+});
+
+document.getElementById('github').addEventListener('click', function() {
+    window.location.href = 'https://github.com/SirBrandolf'; 
+});
